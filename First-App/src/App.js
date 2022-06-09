8b8b895e1faec4dba18dfe97db4ac203ef5a1f44
chrisAXZA <nexter1@tutanota.com>
2022-06-09 18:16:08 +0300

    Move pricingPlans to separate component

--- First-App/src/App.js
+++ First-App/src/App.js
@@ -3,6 +3,8 @@ import Footer from "./components/Footer.js";
 import Header from "./components/Header.js";
 import OurServices from "./components/OurServices.js";
 import OurServices2 from "./components/OurServices2.js";
+import PricingPlans from "./components/PricingPlans.js";
+import Search from "./components/Search.js";
 
 function App() {
   return (
@@ -17,102 +19,9 @@ function App() {
 
       <OurServices2 />
 
-      <div className="page-section banner-seo-check">
-        <div className="wrap bg-image" style={{ backgroundImage: 'url(/img/bg_pattern.svg)' }}>
-          <div className="container text-center">
-            <div className="row justify-content-center wow fadeInUp">
-              <div className="col-lg-8">
-                <h2 className="mb-4">Check your Website SEO</h2>
-                <form action="#">
-                  <input type="text" className="form-control" placeholder="E.g google.com" />
-                  <button type="submit" className="btn btn-success">Check Now</button>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Search />
 
-      <div className="page-section">
-        <div className="container">
-          <div className="text-center wow fadeInUp">
-            <div className="subhead">Pricing Plan</div>
-            <h2 className="title-section">Choose plan the right for you</h2>
-            <div className="divider mx-auto"></div>
-          </div>
-          <div className="row mt-5">
-            <div className="col-lg-4 py-3 wow zoomIn">
-              <div className="card-pricing">
-                <div className="header">
-                  <div className="pricing-type">Basic</div>
-                  <div className="price">
-                    <span className="dollar">$</span>
-                    <h1>39<span className="suffix">.99</span></h1>
-                  </div>
-                  <h5>Per Month</h5>
-                </div>
-                <div className="body">
-                  <p>25 Analytics <span className="suffix">Campaign</span></p>
-                  <p>1,300 Change <span className="suffix">Keywords</span></p>
-                  <p>Social Media <span className="suffix">Reviews</span></p>
-                  <p>1 Free <span className="suffix">Optimization</span></p>
-                  <p>24/7 <span className="suffix">Support</span></p>
-                </div>
-                <div className="footer">
-                  <a href="#" className="btn btn-pricing btn-block">Subscribe</a>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-lg-4 py-3 wow zoomIn">
-              <div className="card-pricing marked">
-                <div className="header">
-                  <div className="pricing-type">Standar</div>
-                  <div className="price">
-                    <span className="dollar">$</span>
-                    <h1>59<span className="suffix">.99</span></h1>
-                  </div>
-                  <h5>Per Month</h5>
-                </div>
-                <div className="body">
-                  <p>25 Analytics <span className="suffix">Campaign</span></p>
-                  <p>1,300 Change <span className="suffix">Keywords</span></p>
-                  <p>Social Media <span className="suffix">Reviews</span></p>
-                  <p>1 Free <span className="suffix">Optimization</span></p>
-                  <p>24/7 <span className="suffix">Support</span></p>
-                </div>
-                <div className="footer">
-                  <a href="#" className="btn btn-pricing btn-block">Subscribe</a>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-lg-4 py-3 wow zoomIn">
-              <div className="card-pricing">
-                <div className="header">
-                  <div className="pricing-type">Professional</div>
-                  <div className="price">
-                    <span className="dollar">$</span>
-                    <h1>99<span className="suffix">.99</span></h1>
-                  </div>
-                  <h5>Per Month</h5>
-                </div>
-                <div className="body">
-                  <p>25 Analytics <span className="suffix">Campaign</span></p>
-                  <p>1,300 Change <span className="suffix">Keywords</span></p>
-                  <p>Social Media <span className="suffix">Reviews</span></p>
-                  <p>1 Free <span className="suffix">Optimization</span></p>
-                  <p>24/7 <span className="suffix">Support</span></p>
-                </div>
-                <div className="footer">
-                  <a href="#" className="btn btn-pricing btn-block">Subscribe</a>
-                </div>
-              </div>
-            </div>
-
-          </div>
-        </div>
-      </div>
+      <PricingPlans />
 
       <div className="page-section banner-info">
         <div className="wrap bg-image" style={{ backgroundImage: 'url(/img/bg_pattern.svg)' }}>

4a9a381a0c526c48801db2603a871758db3a9ef1
chrisAXZA <nexter1@tutanota.com>
2022-06-09 18:36:26 +0300

    Move serviceCards into subComponents

--- First-App/src/App.js
+++ First-App/src/App.js
@@ -1,11 +1,12 @@
-import AboutUs from "./components/AboutUs.js";
 import Footer from "./components/Footer.js";
 import Header from "./components/Header.js";
+import Search from "./components/Search.js";
+import AboutUs from "./components/AboutUs.js";
+import Mission from "./components/Mission.js";
 import LatestNews from "./components/LatestNews.js";
 import OurServices from "./components/OurServices.js";
 import OurServices2 from "./components/OurServices2.js";
 import PricingPlans from "./components/PricingPlans.js";
-import Search from "./components/Search.js";
 
 function App() {
   return (
---
+++ First-App/src/components/ServiceCard1.js
@@ -0,0 +1,18 @@
+function ServiceCard1() {
+    return (
+        <div className="col-lg-4">
+            <div className="card-service wow fadeInUp">
+                <div className="header">
+                    <img src="/img/services/service-1.svg" alt="" />
+                </div>
+                <div className="body">
+                    <h5 className="text-secondary">SEO Consultancy</h5>
+                    <p>We help you define your SEO objective & develop a realistic strategy with you</p>
+                    <a href="service.html" className="btn btn-primary">Read More</a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ServiceCard1;

76b07a439bab11992e827c86731854a55acc48ac
chrisAXZA <nexter1@tutanota.com>
2022-06-09 17:35:49 +0300

    Move ourservices into separate component

--- First-App/src/App.js
+++ First-App/src/App.js
@@ -1,5 +1,6 @@
 import Footer from "./components/Footer.js";
 import Header from "./components/Header.js";
+import OurServices from "./components/OurServices.js";
 
 function App() {
   return (
@@ -8,48 +9,7 @@ function App() {
 
       <Header />
 
-      <div className="page-section">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-4">
-              <div className="card-service wow fadeInUp">
-                <div className="header">
-                  <img src="/img/services/service-1.svg" alt="" />
-                </div>
-                <div className="body">
-                  <h5 className="text-secondary">SEO Consultancy</h5>
-                  <p>We help you define your SEO objective & develop a realistic strategy with you</p>
-                  <a href="service.html" className="btn btn-primary">Read More</a>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-4">
-              <div className="card-service wow fadeInUp">
-                <div className="header">
-                  <img src="/img/services/service-2.svg" alt="" />
-                </div>
-                <div className="body">
-                  <h5 className="text-secondary">Content Marketing</h5>
-                  <p>We help you define your SEO objective & develop a realistic strategy with you</p>
-                  <a href="service.html" className="btn btn-primary">Read More</a>
-                </div>
-              </div>
-            </div>
-            <div className="col-lg-4">
-              <div className="card-service wow fadeInUp">
-                <div className="header">
-                  <img src="/img/services/service-3.svg" alt="" />
-                </div>
-                <div className="body">
-                  <h5 className="text-secondary">Keyword Research</h5>
-                  <p>We help you define your SEO objective & develop a realistic strategy with you</p>
-                  <a href="service.html" className="btn btn-primary">Read More</a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <OurServices />
 
       <div className="page-section" id="about">
         <div className="container">

0e25af762be02e5f4644baafd83d87c4dd893f34
chrisAXZA <nexter1@tutanota.com>
2022-06-09 18:21:53 +0300

    Move mission + latestNews into separate comps

--- First-App/src/App.js
+++ First-App/src/App.js
@@ -1,6 +1,7 @@
 import AboutUs from "./components/AboutUs.js";
 import Footer from "./components/Footer.js";
 import Header from "./components/Header.js";
+import LatestNews from "./components/LatestNews.js";
 import OurServices from "./components/OurServices.js";
 import OurServices2 from "./components/OurServices2.js";
 import PricingPlans from "./components/PricingPlans.js";
@@ -23,93 +24,9 @@ function App() {
 
       <PricingPlans />
 
-      <div className="page-section banner-info">
-        <div className="wrap bg-image" style={{ backgroundImage: 'url(/img/bg_pattern.svg)' }}>
-          <div className="container">
-            <div className="row align-items-center">
-              <div className="col-lg-6 py-3 pr-lg-5 wow fadeInUp">
-                <h2 className="title-section">SEO to Improve Brand <br /> Visibility</h2>
-                <div className="divider"></div>
-                <p>We're an experienced and talented team of passionate consultants who breathe with search engine marketing.</p>
+      <Mission />
 
-                <ul className="theme-list theme-list-light text-white">
-                  <li>
-                    <div className="h5">SEO Content Strategy</div>
-                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor</p>
-                  </li>
-                  <li>
-                    <div className="h5">B2B SEO</div>
-                    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor</p>
-                  </li>
-                </ul>
-              </div>
-              <div className="col-lg-6 py-3 wow fadeInRight">
-                <div className="img-fluid text-center">
-                  <img src="/img/banner_image_2.svg" alt="" />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="page-section">
-        <div className="container">
-          <div className="text-center wow fadeInUp">
-            <div className="subhead">Our Blog</div>
-            <h2 className="title-section">Read Latest News</h2>
-            <div className="divider mx-auto"></div>
-          </div>
-
-          <div className="row mt-5">
-            <div className="col-lg-4 py-3 wow fadeInUp">
-              <div className="card-blog">
-                <div className="header">
-                  <div className="post-thumb">
-                    <img src="/img/blog/blog-1.jpg" alt="" />
-                  </div>
-                </div>
-                <div className="body">
-                  <h5 className="post-title"><a href="#">Source of Content Inspiration</a></h5>
-                  <div className="post-date">Posted on <a href="#">27 Jan 2020</a></div>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-lg-4 py-3 wow fadeInUp">
-              <div className="card-blog">
-                <div className="header">
-                  <div className="post-thumb">
-                    <img src="/img/blog/blog-2.jpg" alt="" />
-                  </div>
-                </div>
-                <div className="body">
-                  <h5 className="post-title"><a href="#">Source of Content Inspiration</a></h5>
-                  <div className="post-date">Posted on <a href="#">27 Jan 2020</a></div>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-lg-4 py-3 wow fadeInUp">
-              <div className="card-blog">
-                <div className="header">
-                  <div className="post-thumb">
-                    <img src="/img/blog/blog-3.jpg" alt="" />
-                  </div>
-                </div>
-                <div className="body">
-                  <h5 className="post-title"><a href="#">Source of Content Inspiration</a></h5>
-                  <div className="post-date">Posted on <a href="#">27 Jan 2020</a></div>
-                </div>
-              </div>
-            </div>
-
-            <div className="col-12 mt-4 text-center wow fadeInUp">
-              <a href="blog.html" className="btn btn-primary">View More</a>
-            </div>
-          </div>
-        </div>
-      </div>
+      <LatestNews />
 
       <Footer />
     </div>

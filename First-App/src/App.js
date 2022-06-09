845741f4cbc23e308cd76b5078e7820b89a32e94
chrisAXZA <nexter1@tutanota.com>
2022-06-09 19:43:54 +0300

    Rename topServices

--- First-App/src/App.js
+++ First-App/src/App.js
@@ -4,7 +4,7 @@ import Search from "./components/Search.js";
 import AboutUs from "./components/AboutUs.js";
 import Mission from "./components/Mission.js";
 import LatestNews from "./components/LatestNews.js";
-import OurServices from "./components/OurServices.js";
+import TopServices from "./components/TopServices.js";
 import OurServices2 from "./components/OurServices2.js";
 import PricingPlans from "./components/PricingPlans.js";
 
@@ -15,7 +15,7 @@ function App() {
 
       <Header />
 
-      <OurServices />
+      <TopServices />
 
       <AboutUs />
 

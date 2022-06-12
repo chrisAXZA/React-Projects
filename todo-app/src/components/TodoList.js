fc80c3296a183603944918f266ac029161427bca
chrisAXZA <nexter1@tutanota.com>
2022-06-12 15:53:41 +0300

    Variable to pass as props

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,20 +1,23 @@
 import TodoListItem from "./TodoListItem.js";
 
 function TodoList() {
+    let firstTask = 'Clean room';
+
     return (
         <>
             <h2>Tasks</h2>
 
             <ul>
-                <TodoListItem>Clean room</TodoListItem>
-                <TodoListItem>Go to the shopping mall</TodoListItem>
-                <TodoListItem>Upgrade coding skills</TodoListItem>
-                <TodoListItem>Cook a meal</TodoListItem>
-                {/* <TodoListItem text="Clean room" /> */}
-                {/* <TodoListItem text="Go to the shopping mall" /> */}
-                {/* <TodoListItem text="Upgrade coding skills" /> */}
-                {/* <TodoListItem text="Cook a meal" /> */}
+                <TodoListItem color="green">{firstTask}</TodoListItem>
+                <TodoListItem color="red">Go to the shopping mall</TodoListItem>
+                <TodoListItem color="blue">Upgrade coding skills</TodoListItem>
+                <TodoListItem color="black">Cook a meal</TodoListItem>
 
+                {/* <TodoListItem><p>Clean room</p><p>Clean room2</p></TodoListItem> */}
+                {/* <TodoListItem text="Clean room" > */}
+                {/* <TodoListItem text="Go to the shopping mall" > */}
+                {/* <TodoListItem text="Upgrade coding skills" > */}
+                {/* <TodoListItem text="Cook a meal" > */}
 
                 {/* React.createElement(TodoListItem, {text:"Cook a meal"}) */}
                 {/* <li>Clean room</li>
--- todo-app/src/components/TodoListItem.js
+++ todo-app/src/components/TodoListItem.js
@@ -1,7 +1,7 @@
 const TodoListItem = (props) => {
     // console.log(props);
     // return <li>{props.text}</li>;
-    return <li>{props.children}</li>;
+    return <li style={{ color: props.color }}>{props.children}</li>;
 }
 
 export default TodoListItem;

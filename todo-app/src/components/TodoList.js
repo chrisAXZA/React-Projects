0921b3234cc15d4d1d30239c7e8cb1d60a5bb941
chrisAXZA <nexter1@tutanota.com>
2022-06-12 12:07:08 +0300

    Implement functional component variant

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,16 +1,29 @@
-import React from 'react';
-
-class TodoList extends React.Component {
-    render() {
-        return (
-            <ul>
-                <li>Clean room</li>
-                <li>Go to the shopping mall</li>
-                <li>Upgrade coding skills</li>
-                <li>Cook a meal</li>
-            </ul>
-        );
-    }
+function TodoList() {
+    return (
+        <ul>
+            <li>Clean room</li>
+            <li>Go to the shopping mall</li>
+            <li>Upgrade coding skills</li>
+            <li>Cook a meal</li>
+        </ul>
+    );
 }
 
 export default TodoList;
+
+
+// Class demo
+// import React from 'react';
+
+// class TodoList extends React.Component {
+//     render() {
+//         return (
+//             <ul>
+//                 <li>Clean room</li>
+//                 <li>Go to the shopping mall</li>
+//                 <li>Upgrade coding skills</li>
+//                 <li>Cook a meal</li>
+//             </ul>
+//         );
+//     }
+// }

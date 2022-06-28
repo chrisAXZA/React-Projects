d45604fd4b1cf5e87291d12754e6aab7b8ca58a3
chrisAXZA <nexter1@tutanota.com>
2022-06-28 20:14:26 +0300

    Implement deleteItem + addItem handlers

--- todo-app2/src/components/TodoItem.js
+++ todo-app2/src/components/TodoItem.js
@@ -1,9 +1,19 @@
-function TodoItem({ text }) {
+function TodoItem({
+    id,
+    text,
+    onDelete,
+}) {
     // console.log(text);
+
+    // const removeTaskHandler = () => console.log('Task removed!');
 
     return (
         // <li>Task - {todo.text}</li>
-        <li>Task - {text}</li>
+        <li>
+            Task - {text}
+            <button onClick={() => onDelete(id)}>Task Completed</button>
+            {/* <button onClick={onDelete}>Task Completed</button> */}
+        </li>
     );
 }
 
--- todo-app2/src/components/TodoList.js
+++ todo-app2/src/components/TodoList.js
@@ -1,19 +1,85 @@
+import { useState } from 'react';
+
 import TodoItem from "./TodoItem.js";
 
 function TodoList() {
-    const todos = [
+    const todoList = [
         { id: 1, text: 'Clean up the apartement' },
         { id: 2, text: 'Fix the heating' },
         { id: 3, text: 'Buy groceries' },
     ];
 
-    // will render todoList item 
+    const [todos, setTodos] = useState(todoList);
+
+    // console.log('render');
+
+    // takes up function as first parameter and dependency-array as second parameter
+    // Function will be executed depending on the conditions specified inside of the array
+    // when empty function will be executed only once when given component is being mounted
+    // useEffect(() => {
+    //     console.log('>>> TodoList Mounted');
+    // }, []);
+
+    const todoAddBlurHandler = (event) => {
+        if (!event.target.value) {
+            return;
+        }
+
+        let todo = {
+            id: todos.length + 1,
+            text: event.target.value,
+        };
+
+        // Setter not only takes up value, but also function
+        // first variant in updating state
+        // setTodos([
+        //     ...todos,
+        //     todo,
+        // ]);
+
+        // second variant with state which indicate that current state is being modified,
+        // meaning current collection of todos that is being modified instead of taking collection
+        // in useState declaration (which might not be up to date)
+        setTodos((state) =>
+            // reference to state is not changed, component will not be re-rendered       
+            // {
+            //     state.push(todo);
+            //     console.log(state);
+            //     return state;
+            // }
+
+            [
+                ...state,
+                todo,
+            ]
+        );
+
+        event.target.value = '';
+    };
+
+    const deleteTodoItemHandler = (id) => {
+        // console.log('Remove task with id >>> ', id);
+        // setTodos((state) => [...state].filter((x) => x.id !== id));
+        
+        setTodos((state) => state.filter((x) => x.id !== id));
+    };
+
+    // will render todoList item => Race Condition Problem
     return (
-        <ul>
-            {/* {todos.map(({ id, text }) => <li key={id}>{text}</li>)} */}
-            {/* {todos.map((todo) => <TodoItem key={todo.id} todo={todo} />)} */}
-            {todos.map((todo) => <TodoItem key={todo.id} text={todo.text} />)}
-        </ul>
+        <>
+            <label htmlFor="todo-name">Add Todo</label>
+            <input id="todo-name" type="text" onBlur={todoAddBlurHandler} name="todo" />
+            {/* <button onClick={todoAddClickHandler}>Add Todo</button> */}
+            <ul>
+                {todos.map((todo) => <TodoItem key={todo.id} id={todo.id} text={todo.text} onDelete={deleteTodoItemHandler} />)}
+                {/* {todos.map((todo) => <TodoItem key={todo.id} id={todo.id} text={todo.text} onDelete={() => deleteTodoItemHandler(todo.id)} />)} */}
+                {/* {todos.map((todo) => <TodoItem key={todo.id} id={todo.id} text={todo.text} onDelete={deleteTodoItemHandler.bind(null, todo.id)} />)} */}
+
+                {/* <TodoItem key={1} text={'Pesho is back'} /> */}
+                {/* {todos.map(({ id, text }) => <li key={id}>{text}</li>)} */}
+                {/* {todos.map((todo) => <TodoItem key={todo.id} todo={todo} />)} */}
+            </ul>
+        </>
     );
 }
 

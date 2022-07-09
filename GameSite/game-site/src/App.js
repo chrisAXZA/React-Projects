035fb85be025cb9bbffcde34e6c1648ed4080705
chrisAXZA <nexter1@tutanota.com>
2022-07-09 12:47:28 +0300

    Add GameCatalog component

--- GameSite/game-site/src/App.js
+++ GameSite/game-site/src/App.js
@@ -1,5 +1,6 @@
 import CreateGame from "./components/CreateGame.js";
 import EditGame from "./components/EditGame.js";
+import GameCatalog from "./components/GameCatalog.js";
 import GameDetails from "./components/GameDetails.js";
 import Header from "./components/Header.js";
 import Login from "./components/Login.js";
@@ -24,44 +25,10 @@ function App() {
                 <EditGame />
 
                 <GameDetails />
+
+                <GameCatalog />
             </main>
 
-
-
-
-
-
-            <section id="catalog-page">
-                <h1>All Games</h1>
-                <div className="allGames">
-                    <div className="allGames-info">
-                        <img src="/images/avatar-1.jpg" />
-                        <h6>Action</h6>
-                        <h2>Cover Fire</h2>
-                        <a href="#" className="details-button">Details</a>
-                    </div>
-
-                </div>
-                <div className="allGames">
-                    <div className="allGames-info">
-                        <img src="/images/avatar-1.jpg" />
-                        <h6>Action</h6>
-                        <h2>Zombie lang</h2>
-                        <a href="#" className="details-button">Details</a>
-                    </div>
-
-                </div>
-                <div className="allGames">
-                    <div className="allGames-info">
-                        <img src="/images/avatar-1.jpg" />
-                        <h6>Action</h6>
-                        <h2>MineCraft</h2>
-                        <a href="#" className="details-button">Details</a>
-                    </div>
-                </div>
-
-                <h3 className="no-articles">No articles yet</h3>
-            </section>
         </div>
     );
 }

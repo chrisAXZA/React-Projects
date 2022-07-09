db0c0874d468d1114594ce456106198eb20fda4b
chrisAXZA <nexter1@tutanota.com>
2022-07-09 12:39:23 +0300

    Add GameDetails component

--- GameSite/game-site/src/App.js
+++ GameSite/game-site/src/App.js
@@ -1,5 +1,6 @@
 import CreateGame from "./components/CreateGame.js";
 import EditGame from "./components/EditGame.js";
+import GameDetails from "./components/GameDetails.js";
 import Header from "./components/Header.js";
 import Login from "./components/Login.js";
 import Register from "./components/Register.js";
@@ -21,57 +22,14 @@ function App() {
                 <CreateGame />
 
                 <EditGame />
+
+                <GameDetails />
             </main>
 
 
 
 
-            <section id="game-details">
-                <h1>Game Details</h1>
-                <div className="info-section">
 
-                    <div className="game-header">
-                        <img className="game-img" src="images/MineCraft.png" />
-                        <h1>Bright</h1>
-                        <span className="levels">MaxLevel: 4</span>
-                        <p className="type">Action, Crime, Fantasy</p>
-                    </div>
-
-                    <p className="text">
-                        Set in a world where fantasy creatures live side by side with humans. A human cop is forced to work
-                        with an Orc to find a weapon everyone is prepared to kill for. Set in a world where fantasy
-                        creatures live side by side with humans. A human cop is forced
-                        to work with an Orc to find a weapon everyone is prepared to kill for.
-                    </p>
-
-                    <div className="details-comments">
-                        <h2>Comments:</h2>
-                        <ul>
-                            <li className="comment">
-                                <p>Content: I rate this one quite highly.</p>
-                            </li>
-                            <li className="comment">
-                                <p>Content: The best game.</p>
-                            </li>
-                        </ul>
-                        <p className="no-comment">No comments.</p>
-                    </div>
-
-                    <div className="buttons">
-                        <a href="#" className="button">Edit</a>
-                        <a href="#" className="button">Delete</a>
-                    </div>
-                </div>
-
-                <article className="create-comment">
-                    <label>Add new comment:</label>
-                    <form className="form">
-                        <textarea name="comment" placeholder="Comment......"></textarea>
-                        <input className="btn submit" type="submit" value="Add Comment" />
-                    </form>
-                </article>
-
-            </section>
 
             <section id="catalog-page">
                 <h1>All Games</h1>

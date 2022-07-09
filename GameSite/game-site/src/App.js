7f658677e2a3268bf20d3427a5f8d67dd7efb9c5
chrisAXZA <nexter1@tutanota.com>
2022-07-09 12:27:28 +0300

    Add Register component

--- GameSite/game-site/src/App.js
+++ GameSite/game-site/src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header.js";
 import Login from "./components/Login.js";
+import Register from "./components/Register.js";
 import WelcomeWorld from "./components/WelcomeWorld.js";
 
 function App() {
@@ -12,34 +13,14 @@ function App() {
                 <WelcomeWorld />
 
                 <Login />
+
+                <Register />
             </main>
 
 
 
 
-            <section id="register-page" className="content auth">
-                <form id="register">
-                    <div className="container">
-                        <div className="brand-logo"></div>
-                        <h1>Register</h1>
 
-                        <label htmlFor="email">Email:</label>
-                        <input type="email" id="email" name="email" placeholder="maria@email.com" />
-
-                        <label htmlFor="pass">Password:</label>
-                        <input type="password" name="password" id="register-password" />
-
-                        <label htmlFor="con-pass">Confirm Password:</label>
-                        <input type="password" name="confirm-password" id="confirm-password" />
-
-                        <input className="btn submit" type="submit" value="Register" />
-
-                        <p className="field">
-                            <span>If you already have profile click <a href="#">here</a></span>
-                        </p>
-                    </div>
-                </form>
-            </section>
 
             <section id="create-page" className="auth">
                 <form id="create">

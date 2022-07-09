133db513caf258cbcd918a1036e5bc54b98246c1
chrisAXZA <nexter1@tutanota.com>
2022-07-09 12:37:18 +0300

    Add EditGame component

--- GameSite/game-site/src/App.js
+++ GameSite/game-site/src/App.js
@@ -1,4 +1,5 @@
 import CreateGame from "./components/CreateGame.js";
+import EditGame from "./components/EditGame.js";
 import Header from "./components/Header.js";
 import Login from "./components/Login.js";
 import Register from "./components/Register.js";
@@ -18,33 +19,12 @@ function App() {
                 <Register />
 
                 <CreateGame />
+
+                <EditGame />
             </main>
 
 
-            <section id="edit-page" className="auth">
-                <form id="edit">
-                    <div className="container">
 
-                        <h1>Edit Game</h1>
-                        <label htmlFor="leg-title">Legendary title:</label>
-                        <input type="text" id="title" name="title" />
-
-                        <label htmlFor="category">Category:</label>
-                        <input type="text" id="category" name="category" />
-
-                        <label htmlFor="levels">MaxLevel:</label>
-                        <input type="number" id="maxLevel" name="maxLevel" min="1" />
-
-                        <label htmlFor="game-img">Image:</label>
-                        <input type="text" id="imageUrl" name="imageUrl" />
-
-                        <label htmlFor="summary">Summary:</label>
-                        <textarea name="summary" id="summary"></textarea>
-                        <input className="btn submit" type="submit" value="Edit Game" />
-
-                    </div>
-                </form>
-            </section>
 
             <section id="game-details">
                 <h1>Game Details</h1>

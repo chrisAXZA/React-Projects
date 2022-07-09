ee3d4b9600ad6a29d4a86e8523c6e12cb2d57dd3
chrisAXZA <nexter1@tutanota.com>
2022-07-09 12:25:29 +0300

    Add Login component

--- GameSite/game-site/src/App.js
+++ GameSite/game-site/src/App.js
@@ -1,4 +1,5 @@
 import Header from "./components/Header.js";
+import Login from "./components/Login.js";
 import WelcomeWorld from "./components/WelcomeWorld.js";
 
 function App() {
@@ -8,28 +9,13 @@ function App() {
             <Header />
 
             <main id="main-content">
+                <WelcomeWorld />
+
+                <Login />
             </main>
 
-            <WelcomeWorld />
 
-            <section id="login-page" className="auth">
-                <form id="login">
 
-                    <div className="container">
-                        <div className="brand-logo"></div>
-                        <h1>Login</h1>
-                        <label htmlFor="email">Email:</label>
-                        <input type="email" id="email" name="email" placeholder="Sokka@gmail.com" />
-
-                        <label htmlFor="login-pass">Password:</label>
-                        <input type="password" id="login-password" name="password" />
-                        <input type="submit" className="btn submit" value="Login" />
-                        <p className="field">
-                            <span>If you don't have profile click <a href="#">here</a></span>
-                        </p>
-                    </div>
-                </form>
-            </section>
 
             <section id="register-page" className="content auth">
                 <form id="register">

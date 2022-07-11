005faf59e162c8f9634aa9d4c5f342222d0bc9e2
chrisAXZA <nexter1@tutanota.com>
2022-07-11 10:18:40 +0300

    Implement isLoading funcionality

--- GameSite/game-site/src/components/GameCatalog/GameCatalog.js
+++ GameSite/game-site/src/components/GameCatalog/GameCatalog.js
@@ -3,12 +3,14 @@ import GameCard from './GameCard.js';
 
 const GameCatalog = () => {
     const [games, setGames] = useState([]);
+    const [loading, setLoading] = useState(false);
 
     // setTimeout(() => {
     // // useEffect here for delayed loading
     // }, 1000);
 
     useEffect(() => {
+        setLoading(true);
         fetch('http://localhost:3030/data/games?sortBy=_createdOn%20desc')
             .then((res) => {
                 // console.log(res);
@@ -17,17 +19,22 @@ const GameCatalog = () => {
             .then((result) => {
                 // set all games present on backend unto local games-state
                 setGames(result);
-            })
-            ;
+                setLoading(false);
+            });
     }, []);
 
     return (
         <section id="catalog-page">
             <h1>All Games</h1>
 
-            {games.map((game) => <GameCard key={game._id} game={game} />)}
+            {
+                games.length > 0
+                    ? games.map((game) => <GameCard key={game._id} game={game} />)
+                    : <h3 className="no-articles">No games yet</h3>
+            }
 
-            <h3 className="no-articles">No games yet</h3>
+            {/* { loading ? <h1>Loading...</h1> : games.length > 0 ? games.map((game) => <GameCard key={game._id} game={game} />) : <h3 className="no-articles">No games yet</h3> } */}
+
         </section>
     );
 };

23d25b89fa3769c5238bc6f5667527e3b74b18cc
chrisAXZA <nexter1@tutanota.com>
2022-07-24 22:07:56 +0300

    Add Header component

---
+++ Pets-App/pets-app/src/components/Header/index.js
@@ -0,0 +1,5 @@
+export { default } from './Header.js';
+
+// standard import/export
+// import Header from "./Header.js";
+// export default Header;
---
+++ Pets-App/pets-app/src/components/index.js
@@ -0,0 +1,7 @@
+import HeaderComponent from './Header';
+
+export const Header = HeaderComponent;
+
+// second variant
+// import Header from './Header';
+// export default {Header};

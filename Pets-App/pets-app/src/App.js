6c7f46ecc3bc6d2a889c651bee748025ae1d48c2
chrisAXZA <nexter1@tutanota.com>
2022-07-25 19:31:16 +0300

    Add MyPets component

--- Pets-App/pets-app/src/App.js
+++ Pets-App/pets-app/src/App.js
@@ -4,6 +4,7 @@ import Details from "./components/Details/Details.js";
 import Edit from "./components/Edit/Edit.js";
 import Header from "./components/Header";
 import Login from "./components/Login";
+import MyPets from "./components/MyPets/MyPets.js";
 import Register from "./components/Register/Register.js";
 
 function App() {
@@ -26,25 +27,7 @@ function App() {
 
             {<Edit />}
 
-            <section id="my-pets-page" className="my-pets">
-                <h1>My Pets</h1>
-                <ul className="my-pets-list">
-                    <li className="otherPet">
-                        <h3>Name: Milo</h3>
-                        <p>Type: dog</p>
-                        <p className="img"><img src="/images/dog.png" /></p>
-                        <a className="button" href="#">Details</a>
-                    </li>
-                    <li className="otherPet">
-                        <h3>Name: Tom</h3>
-                        <p>Type: cat</p>
-                        <p className="img"><img src="/images/cat1.png" /></p>
-                        <a className="button" href="#">Details</a>
-                    </li>
-                </ul>
-
-                <p className="no-pets">No pets in database!</p>
-            </section>
+            {<MyPets />}
 
             <footer id="site-footer">
                 <p>@PetMyPet</p>

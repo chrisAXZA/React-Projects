8f14bffdd9db55b3a85a27fc58b77115b8b05bd3
chrisAXZA <nexter1@tutanota.com>
2022-07-25 19:02:05 +0300

    Add Create component

--- Pets-App/pets-app/src/App.js
+++ Pets-App/pets-app/src/App.js
@@ -1,3 +1,4 @@
+import Create from "./components/Create/Create.js";
 import Dashboard from "./components/Dashboard/Dashboard.js";
 import Details from "./components/Details/Details.js";
 import Header from "./components/Header";
@@ -20,44 +21,7 @@ function App() {
 
             {<Details />}
 
-            <section id="create-page" className="create">
-                <form id="create-form" action="" method="">
-                    <fieldset>
-                        <legend>Add new Pet</legend>
-                        <p className="field">
-                            <label htmlFor="name">Name</label>
-                            <span className="input">
-                                <input type="text" name="name" id="name" placeholder="Name" />
-                            </span>
-                        </p>
-                        <p className="field">
-                            <label htmlFor="description">Description</label>
-                            <span className="input">
-                                <textarea name="description" id="description" placeholder="Description"></textarea>
-                            </span>
-                        </p>
-                        <p className="field">
-                            <label htmlFor="image">Image</label>
-                            <span className="input">
-                                <input type="text" name="imageUrl" id="image" placeholder="Image" />
-                            </span>
-                        </p>
-                        <p className="field">
-                            <label htmlFor="type">Type</label>
-                            <span className="input">
-                                <select id="type" name="type">
-                                    <option value="cat">Cat</option>
-                                    <option value="dog">Dog</option>
-                                    <option value="parrot">Parrot</option>
-                                    <option value="reptile">Reptile</option>
-                                    <option value="other">Other</option>
-                                </select>
-                            </span>
-                        </p>
-                        <input className="button submit" type="submit" value="Add Pet" />
-                    </fieldset>
-                </form>
-            </section>
+            {<Create />}
 
             <section id="edit-page" className="edit">
                 <form id="edit-form" action="#" method="">

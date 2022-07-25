c12a0f0d362677cbeb11551aaa4e1e1fc1d21258
chrisAXZA <nexter1@tutanota.com>
2022-07-25 18:46:36 +0300

    Add Register component

--- Pets-App/pets-app/src/App.js
+++ Pets-App/pets-app/src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Login from "./components/Login";
+import Register from "./components/Register/Register.js";
 
 function App() {
     return (
@@ -11,32 +12,7 @@ function App() {
 
             {<Login />}
 
-            <section id="register-page" className="register">
-                <form id="register-form" action="" method="">
-                    <fieldset>
-                        <legend>Register Form</legend>
-                        <p className="field">
-                            <label htmlFor="email">Email</label>
-                            <span className="input">
-                                <input type="text" name="email" id="email" placeholder="Email" />
-                            </span>
-                        </p>
-                        <p className="field">
-                            <label htmlFor="password">Password</label>
-                            <span className="input">
-                                <input type="password" name="password" id="password" placeholder="Password" />
-                            </span>
-                        </p>
-                        <p className="field">
-                            <label htmlFor="repeat-pass">Repeat Password</label>
-                            <span className="input">
-                                <input type="password" name="confirm-pass" id="repeat-pass" placeholder="Repeat Password" />
-                            </span>
-                        </p>
-                        <input className="button submit" type="submit" value="Register" />
-                    </fieldset>
-                </form>
-            </section>
+            {<Register />}
 
             <section id="dashboard-page" className="dashboard">
                 <h1>Dashboard</h1>

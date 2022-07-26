7bc09ee13761e477deb74c2537db56c3368e6e78
chrisAXZA <nexter1@tutanota.com>
2022-07-26 11:04:58 +0300

    Add authService isAuthenticated functionality

--- Pets-App/pets-app/src/services/authService.js
+++ Pets-App/pets-app/src/services/authService.js
@@ -2,7 +2,11 @@ export const login = (username) => {
     localStorage.setItem('username', username);
 };
 
-export const isAuthenticated = () => {
+export const getUser = () => {
     let username = localStorage.getItem('username');
-    return { username };
+    return username;
+};
+
+export const isAuthenticated = () => {
+    return Boolean(getUser());
 };

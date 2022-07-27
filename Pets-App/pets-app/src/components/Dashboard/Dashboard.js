f1901b81ea86e2300f88a3c510b828f512eb02e9
chrisAXZA <nexter1@tutanota.com>
2022-07-27 10:57:44 +0300

    Add conditional rendering PetCards in Dashboard

--- Pets-App/pets-app/src/components/Dashboard/Dashboard.js
+++ Pets-App/pets-app/src/components/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-// import { Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 // import * as petService from '../../services/petService.js';
 
--- Pets-App/pets-app/src/components/PetList/PetList.js
+++ Pets-App/pets-app/src/components/PetList/PetList.js
@@ -16,10 +16,13 @@ const PetList = () => {
 
     return (
         <>
-            <ul className="other-pets-list">
-                {pets.map((p) => <PetCard key={p._id} pet={p} />)}
-            </ul>
-            <p className="no-pets">No pets in database!</p>
+            {pets.length > 0
+                ? (<ul className="other-pets-list">
+                    {pets.map((p) => <PetCard key={p._id} pet={p} />)}
+                </ul>)
+                : < p className="no-pets">No pets in database!</p>
+            }
+
         </>
     );
 };

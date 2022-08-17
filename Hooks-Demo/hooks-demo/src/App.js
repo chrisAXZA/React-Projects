4ac7aaa24003f1eec36a12e59f689d3778d34954
chrisAXZA <nexter1@tutanota.com>
2022-08-17 15:11:44 +0300

    Add useState demo

--- Hooks-Demo/hooks-demo/src/App.js
+++ Hooks-Demo/hooks-demo/src/App.js
@@ -1,12 +1,37 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    // const stateResult = useState('');
+
+    // const name = stateResult[0];
+    // const setName = stateResult[1];
+
+    const [name, setName] = useState('');
+    const [count, setCount] = useState(1);
+    const [info, setInfo] = useState({
+        name: 'Pesho',
+        age: 30,
+        hobbies: ['first', 'second', 'third',],
+    });
+
+    useEffect(() => {
+        setTimeout(() => {
+            setName('Pesho');
+            setInfo((oldState) => ({ ...oldState, age: 31, }));
+        }, 1500);
+    }, []);
+
+
+    return (
+        <div className="App">
+            <h2>{name || 'Loading...'}</h2>
+            {/* <h2>{!name ? 'Loading...' : name}</h2> */}
+            <h3>{count}</h3>
+            <button onClick={() => setCount((c) => c + 1)}>+</button>
+        </div>
+    );
 }
 
 export default App;

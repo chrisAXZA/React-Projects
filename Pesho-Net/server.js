d7c831a861b66779df0c46b9b80a3ea17c5f575f
chrisAXZA <nexter1@tutanota.com>
2022-09-16 17:11:04 +0300

    Add user.routes.js+routes folder

--- Pesho-Net/server.js
+++ Pesho-Net/server.js
@@ -1,11 +1,16 @@
 import express from 'express';
-import * as dotenv from 'dotenv';
 import db from './config/db.js';
+import * as dotenv from 'dotenv';
+import userRoutes from './routes/user.routes.js';
 
 dotenv.config({ path: './config/.env', });
 
 const app = express();
 
+// routes
+// app.use('/api/user', userRoutes);
+
+// server
 app.listen(process.env.PORT, () => {
     console.log(`App is running on Port >>> ${process.env.PORT}`);
 });

8c48721391f7b66cb25b95d85f27e0e74ebf2d32
chrisAXZA <nexter1@tutanota.com>
2022-09-16 13:20:15 +0300

    Add bcrypt+validator+user.model.js

--- Pesho-Net/server.js
+++ Pesho-Net/server.js
@@ -2,8 +2,9 @@ import express from 'express';
 import * as dotenv from 'dotenv';
 import db from './config/db.js';
 
-const app = express();
 dotenv.config({ path: './config/.env', });
+
+const app = express();
 
 app.listen(process.env.PORT, () => {
     console.log(`App is running on Port >>> ${process.env.PORT}`);

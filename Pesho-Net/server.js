b94c242314f7932ecf20b531999303f5da05608c
chrisAXZA <nexter1@tutanota.com>
2022-09-16 11:52:32 +0300

    Implement dotenv in server.js

--- Pesho-Net/server.js
+++ Pesho-Net/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
+import * as dotenv from 'dotenv';
 
 const app = express();
+dotenv.config({ path: './config/.env', });
 
-app.listen(3000, () => {
-    console.log(`App is running on Port >>> 3000`);
+app.listen(process.env.PORT, () => {
+    console.log(`App is running on Port >>> ${process.env.PORT}`);
 });

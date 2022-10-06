1cc16f2ed3dd89621efbdaca7b7dbb3fe6899887
chrisAXZA <nexter1@tutanota.com>
2022-10-06 21:44:35 +0300

    Implement requireAuth+import in server.js

--- Pesho-Net/middleware/authMiddleware.js
+++ Pesho-Net/middleware/authMiddleware.js
@@ -34,3 +34,26 @@ export const checkUserLogStatus = (req, res, next) => {
         next();
     }
 };
+
+// verifies the current user's token with the ones already found in DB
+export const requireAuth = async (req, res, next) => {
+    const token = req.cookies.jwt;
+
+    if (token) {
+        jwt.verify(
+            token,
+            process.env.TOKEN_SECRET,
+            async (err, decodedToken) => {
+                if (err) {
+                    console.log(err); // no next is being passed and middleware stops the code chain
+                } else {
+                    console.log('Current Token used >>> ' + decodedToken.userId);
+                    // console.log(decodedToken.userId);
+                    next();
+                }
+            }
+        );
+    } else {
+        console.log('Token could not be found!');
+    }
+};
--- Pesho-Net/server.js
+++ Pesho-Net/server.js
@@ -5,7 +5,7 @@ import cookieParser from 'cookie-parser';
 
 import db from './config/db.js';
 import router from './routes/userRoutes.js';
-import { checkUserLogStatus } from './middleware/authMiddleware.js';
+import { checkUserLogStatus, requireAuth } from './middleware/authMiddleware.js';
 
 dotenv.config({ path: './config/.env', });
 
@@ -18,6 +18,12 @@ app.use(cookieParser());
 
 // jwt, for all routes apply checkUserLogStatus to validate whether token and userId is valid
 app.get('*', checkUserLogStatus);
+app.get('/jwtid', requireAuth, (req, res) => {
+    // route will only be used when the user initially access the app
+    // in order to verify whether the user's token is already stored in 
+    // order to automatically register the given user (user will be automatically logged in)
+    res.status(200).send(res.locals.user._id);
+});
 
 // routes
 app.use('/api/user', router);

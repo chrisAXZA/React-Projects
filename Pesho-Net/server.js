bdea4f1d7777a1079f7e1511b5d1069184aad28b
chrisAXZA <nexter1@tutanota.com>
2022-10-06 12:32:19 +0300

    Register cookie-parser as middleware in server.js

--- Pesho-Net/server.js
+++ Pesho-Net/server.js
@@ -1,7 +1,9 @@
 import express from 'express';
-import db from './config/db.js';
 import * as dotenv from 'dotenv';
 import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+
+import db from './config/db.js';
 import router from './routes/userRoutes.js';
 
 dotenv.config({ path: './config/.env', });
@@ -11,6 +13,7 @@ const app = express();
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true, }));
+app.use(cookieParser());
 
 // routes
 app.use('/api/user', router);
